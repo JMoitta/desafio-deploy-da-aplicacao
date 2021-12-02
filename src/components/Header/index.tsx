4de4699b57e00c32e6a93c99a7f714058a52122e
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2021-12-02 17:35:21 -0300

    Initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { SignInButton } from '../SignInButton'
+import logoIgNews from '../../../public/images/logo.svg'
+
+import styles from './styles.module.scss'
+import { useRouter } from 'next/dist/client/router'
+import { ActiveLink } from '../ActiveLink'
+
+export function Header() {
+  const { asPath } = useRouter()
+
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logoIgNews} alt="ig.news" />
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a className={styles.active}>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Post</a>
+          </ActiveLink>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
